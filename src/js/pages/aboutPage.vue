478de77133e64c693be7719c5cbbda5d95d85cc8
Sor Tasnee <warr@outlook.com>
2016-08-02 14:16:50 +0700

    Add more info to about page

--- src/js/pages/aboutPage.vue
+++ src/js/pages/aboutPage.vue
@@ -2,7 +2,20 @@
   <div class="topicWrapper">
     <h1>เกี่ยวกับ Albino</h1>
     <div class="content">
-      พัฒนาโดย <a href="http://twitter.com/warrantica" target="_blank">@warrantica</a>
+      <p>
+        Albino เวอร์ชั่น 0.99-beta2<br>
+        ดูข้อมูลเพิ่มเติมได้ที่<a href="http://warrantica.github.io/albino" target="_blank">เว็บไซต์</a>
+        ติดต่อทีมงานผ่านทวิตเตอร์ <a href="http://twitter.com/albino_reader" target="_blank">@Albino_Reader</a>
+      </p>
+      <p>
+        พัฒนาโดย <a href="http://twitter.com/warrantica" target="_blank">@warrantica</a>
+      </p>
+      <p>
+        Albino คือ open-source software ภายใต้
+        <a href="https://github.com/warrantica/albino/blob/master/LICENSE" target="_blank">GNU General Public License</a>
+        นักพัฒนาหรือบุคคลทั่วไปสามารถเข้าไปดู source code หรือร่วมพัฒนา Albino ได้ที่
+        <a href="https://github.com/warrantica/albino" target="_blank">Github</a>
+      </p>
     </div>
   </div>
 </template>
